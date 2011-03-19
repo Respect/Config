59410f2f87cee8edf9ae7dabc060fdecb70efe27
Alexandre <alexandre@gaigalas.net>
2011-03-19 20:48:15 -0300

    Made Container extends ArrayObject

--- library/Respect/Config/Container.php
+++ library/Respect/Config/Container.php
@@ -4,15 +4,14 @@ namespace Respect\Config;
 
 use UnexpectedValueException;
 use InvalidArgumentException;
-use ArrayAccess;
+use ArrayObject;
 
-class Container implements ArrayAccess
+class Container extends ArrayObject
 {
-
-    protected $items = array();
 
     public function __construct($configurator=null)
     {
+        parent::__construct(array(), static::STD_PROP_LIST | static::ARRAY_AS_PROPS);
         if (is_null($configurator))
             return;
         elseif (is_array($configurator))
@@ -141,11 +140,11 @@ class Container implements ArrayAccess
 
     protected function parseVariables($value)
     {
-        $vars = $this->items;
+        $self = $this;
         return preg_replace_callback(
             '/\[(\w+)\]/',
-            function($match) use($vars) {
-                return $vars[$match[1]] ? : '';
+            function($match) use(&$self) {
+                return $self[$match[1]] ? : '';
             }, $value
         );
     }
@@ -157,73 +156,33 @@ class Container implements ArrayAccess
 
     protected function lazyLoad($name)
     {
-        $callback = $this->items[$name];
-        return $this->items[$name] = $callback();
+        $callback = $this[$name];
+        return $this[$name] = $callback();
     }
 
     public function getItem($name, $raw=false)
     {
         if (!$this->hasItem($name))
             throw new UnexpectedValueException();
-        elseif ($raw || !is_callable($this->items[$name]))
-            return $this->items[$name];
+        elseif ($raw || !is_callable($this[$name]))
+            return $this[$name];
         else
             return $this->lazyLoad($name);
     }
 
     public function setItem($name, $value)
     {
-        $this->items[$name] = $value;
+        return $this->offsetSet($name, $value);
     }
 
     public function removeItem($name)
     {
-        unset($this->items[$name]);
+        return $this->offsetunset($name);
     }
 
     public function hasItem($name)
     {
-        return isset($this->items[$name]);
-    }
-
-    public function __set($name, $value)
-    {
-        return $this->setItem($name, $value);
-    }
-
-    public function __get($name)
-    {
-        return $this->getItem($name);
-    }
-
-    public function __isset($name)
-    {
-        return $this->hasItem($name);
-    }
-
-    public function __unset($name)
-    {
-        return $this->removeItem($name);
-    }
-
-    public function offsetExists($name)
-    {
-        return $this->hasItem($name);
-    }
-
-    public function offsetGet($name)
-    {
-        return $this->getItem($name);
-    }
-
-    public function offsetSet($name, $value)
-    {
-        return $this->setItem($name, $value);
-    }
-
-    public function offsetUnset($name)
-    {
-        return $this->removeItem($name);
+        return $this->offsetExists($name);
     }
 
 }
