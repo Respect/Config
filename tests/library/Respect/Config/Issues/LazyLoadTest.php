7dcfc0094581ef0a885350585a7359bc789e977d
Augusto Pascutti <augusto@phpsp.org.br>
2012-03-23 01:39:22 -0300

    #14 TestCase reproducing the issue

---
+++ tests/library/Respect/Config/Issues/LazyLoadTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace Respect\Config;
+
+class LazyLoadTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @group 	issues
+	 * @ticket 	14
+	 */
+	public function testLazyLoadedParameters()
+	{
+		$config = "
+my_string = 'Hey you!'
+
+[hello Respect\Config\MyLazyLoadedHelloWorld]
+string = [my_string]
+";
+		$expected  = 'Hello World!';
+		$container = new Container($config);
+		$container->my_string = $expected;
+		$this->assertEquals($expected, (string) $container->hello);
+	}
+}
+
+class MyLazyLoadedHelloWorld
+{
+	protected $string;
+
+	public function __construct($string)
+	{
+		$this->string = $string;
+	}
+
+	public function __toString()
+	{
+		return $this->string;
+	}
+}
