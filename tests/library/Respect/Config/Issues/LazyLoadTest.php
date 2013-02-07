ea9f4c63f33bffc2c3337bf4b34ec2583420b5ab
nickl- <github@jigsoft.co.za>
2013-02-07 07:00:57 +0200

    Test for fix for #26 lazy load instance parameters.
    Solution for #26 allow manipulating instances as paramoters.

--- tests/library/Respect/Config/Issues/LazyLoadTest.php
+++ tests/library/Respect/Config/Issues/LazyLoadTest.php
@@ -20,6 +20,43 @@ string = [my_string]
         $container->my_string = $expected;
         $this->assertEquals($expected, (string) $container->hello);
     }
+
+    public function testLazyLoadedInstance()
+    {
+        $config = "
+my_string = 'Hey you!'
+
+[hello Respect\Config\MyLazyLoadedHelloWorld]
+string = [my_string]
+
+[consumer Respect\Config\MyLazyLoadedHelloWorldConsumer]
+hello = [hello]
+    ";
+        $expected  = 'Hello World!';
+        $container = new Container($config);
+        $container->my_string = $expected;
+        $this->assertEquals($expected, (string) $container->hello);
+        $container = new Container($config);
+        $container->{"hello Respect\\Config\\MyLazyLoadedHelloWorld"} = array('string' => $expected);
+        $this->assertEquals($expected, (string) $container->hello);
+        $container = new Container($config);
+        $container->hello = new MyLazyLoadedHelloWorld($expected);
+        $this->assertEquals($expected, (string) $container->hello);
+    }
+}
+class MyLazyLoadedHelloWorldConsumer
+{
+    protected $string;
+
+    public function __construct($hello)
+    {
+        $this->string = $hello;
+    }
+
+    public function __toString()
+    {
+        return $this->string;
+    }
 }
 
 class MyLazyLoadedHelloWorld
